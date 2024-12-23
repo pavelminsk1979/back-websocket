ec8c5e17cbf8f781f40adb9c555ca49597539d57
pavvelpotapov <pavelminsk1979@mail.ru>
2024-12-23 16:15:58 +0300

    finish simple connect

--- src/websocket/admin/admin.service.ts
+++ src/websocket/admin/admin.service.ts
@@ -1,29 +1,21 @@
 import {
   ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
+  SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class SocketAdminService implements OnGatewayConnection {
-  handleEvent(dto: any, @ConnectedSocket() client: any) {
+  @SubscribeMessage('server-path')
+  handleEvent(@MessageBody() dto: any, @ConnectedSocket() client: any) {
     const res = { type: 'someType', dto };
     client.emit('client-path', res);
+    console.log('dto:', dto);
   }
 
   handleConnection(client: any): any {
-    console.log('ПРОИЗОШЛО ПОДКЛЮЧЕНИЕ ПО WEBSOKET');
-    console.log('СОДЕРЖИМОЕ ОБЬЕКТА client');
-
     console.log(client);
   }
-
-  // когда фронтенд будет подключатся к WebSocketСЕРВИСУ
-  // тогда фронтенд отправит запрос на уУСТАНОВЛЕНИЕ СОЕДЕНЕНИЯ
-  // и тогда отработает этот метод
-
-  /* в этом методе например будет ПРОВЕРКА АУТЕНТИФМКАЦИИ Клиента
-  -тоесть его логин и парол проверятся
-  -------ИЛИ ТОКЕН ПРОВЕРИТСЯ
-  ----ИЛИ УВЕДОМИТЬ КАКИХ ЛИБО ПОЛЬЗОВАТЕЛЕЙ О ПОДКЛЮЧЕНИИ*/
 }
